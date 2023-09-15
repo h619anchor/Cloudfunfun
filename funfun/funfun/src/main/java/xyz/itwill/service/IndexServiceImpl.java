56e3a2bb09e5c9bc3951dcfcbbf785ba9bba49e9
H619 <129164514+h619anchor@users.noreply.github.com>
2023-09-15 14:15:03 +0900

    NEW

--- funfun/funfun/src/main/java/xyz/itwill/service/IndexServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package xyz.itwill.service;
-
-public class IndexServiceImpl {
-
-}
--- funfun/funfun/src/main/java/xyz/itwill/service/ShopService.java
+++
@@ -1,8 +0,0 @@
-package xyz.itwill.service;
-
-import java.util.Map;
-
-public interface ShopService {
-
-	Map<String, Object> getAllFestivalList(int pageNum, int pageSize, String selectKeyword);
-}
